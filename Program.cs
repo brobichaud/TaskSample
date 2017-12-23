326236a21d0241e7720eb5aba3afc49cab81f8af
Brett Robichaud <brobichaud@digimarc.com>
2017-12-22 16:13:50 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskSample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var tokenSource = new CancellationTokenSource();
+			var token = tokenSource.Token;
+			List<Task> tasks = new List<Task>();
+			tasks.Add(Task.Factory.StartNew(() => DoWork(token), token));
+			tasks.Add(Task.Factory.StartNew(() => DoWork(token), token));
+			Thread.Sleep(5000);
+
+			tokenSource.Cancel();
+			Task.WaitAll(tasks.ToArray());
+		}
+
+		private static void DoWork(CancellationToken token)
+		{
+			Console.WriteLine("**Processor starting, {0}", Thread.CurrentThread.ManagedThreadId);
+
+			try
+			{
+				while (!token.IsCancellationRequested)
+				{
+					Console.WriteLine("Performing work, {0}...", Thread.CurrentThread.ManagedThreadId);
+					Thread.Sleep(1000);
+				}
+
+				Console.WriteLine("**Processor terminating gracefully, {0}", Thread.CurrentThread.ManagedThreadId);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("**Processor exception, {0}\n" + e, Thread.CurrentThread.ManagedThreadId);
+			}
+		}
+
+	}
+}
